dae82006054d6030e9dc576491e22b796d01497e
sravanikotichukkala <146697443+sravanikotichukkala@users.noreply.github.com>
2024-01-12 10:46:14 +0530

    Update pooh.py

--- pooh.py
+++ pooh.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('pooh')
-st.write('pooh')
+st.write('Ummm...hun🍯 is so yummy!!!!')
 st.image('https://i.pinimg.com/736x/55/27/81/5527816074088de0372a4ed8e53a2da1.jpg',caption='pooh')
